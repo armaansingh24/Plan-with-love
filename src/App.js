5c47a28b72b28d99c7de3ff0cc5eea988e2c2f86
Armaan Singh <sarmaan20720@gmail.com>
2023-03-18 18:49:53 +0530

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { userState } from "react";
+import data from "./Data";
+import Tours from "./components/Tours";
 
-function App() {
+const App = () => {
+  const [tours, setTour] = userState(data);
+
+  function removeTour(id) {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTour(newTours);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Tours tours={tours} removeTour={removeTour}></Tours>
     </div>
   );
-}
+};
 
 export default App;
